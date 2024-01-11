78a976845f573473923238b6e79ac86b5ef25063
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-11 21:38:42 +0530

    Add files via upload

---
+++ add_2no_user_input.py
@@ -0,0 +1,5 @@
+# Write a program in python to calculate addtion of two number but the input takeing by the user
+a=int(input("Enter the 1st number: "))
+b=int(input("Enter the 2nd number: "))
+c=a+b
+print(f"The value is:{c}", )
