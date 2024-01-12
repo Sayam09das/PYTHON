2ef1b93979cc450671139fe5d850aa58340729a6
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-12 21:48:24 +0530

    Add files via upload

---
+++ check_triangle.py
@@ -0,0 +1,9 @@
+import os
+import math
+a=float(input("Enter the value of a: "))
+b=float(input("Enter the value of b: "))
+
+if c == math.sqrt(a) + math.sqrt(b):
+    print("It is a right angle triangle")
+else:
+    print("It is not a right angle triangle")
