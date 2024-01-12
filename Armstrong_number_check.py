6e46422ea93830ba5f28b6136d97c514914a7a1c
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-12 21:47:21 +0530

    Add files via upload

---
+++ Armstrong_number_check.py
@@ -0,0 +1,21 @@
+# Write a program to check whether a number is armstrong or not (153=1^3+5^3+3^3)
+
+import math
+num=int(input("Enter an interger: "))
+n=0
+result=0
+originalnumber=num
+while originalnumber !=0:
+    originalnumber =originalnumber//10
+    n+=1
+
+originalnumber=num
+while originalnumber !=0:
+    remainder=originalnumber%10
+    result+=math.pow(remainder,n)
+    originalnumber=originalnumber//10
+
+if result==num:
+    print(f"{num} is an Armstrong number")
+else:
+        print(f"{num} is not an Armstrong number")
