6b1e70b4744aee64c9872c9fd103b1bdc49f47e5
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-12 21:48:38 +0530

    Add files via upload

---
+++ check_two_number_greater.py
@@ -0,0 +1,9 @@
+# Write a progarm in python to check a number is greater then or lessthen or equal
+x=int(input("Enter the 1st number: "))
+y=int(input("Enter the 2nd number: "))
+if x>y:
+    print(x, "is greater then",y)
+elif x<y:
+    print(x, "is lessthen",y)
+else:
+    print("both are equal",x,y)
