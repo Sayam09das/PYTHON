ecd1ff662c9426b6e77cd1ee58d5361da56dc522
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-16 20:51:58 +0530

    Add files via upload

---
+++ decimal_to_binary.py
@@ -0,0 +1,14 @@
+# Write a program to convert Decimal to Binary
+
+num = int(input("Enter a positive number: "))
+p = 1
+binary_num = 0
+real_number=num
+
+while num > 0:
+    rem = num % 2
+    binary_num = binary_num + rem * p
+    p = p * 10
+    num = num // 2
+
+print(f"Binary number of {real_number} is {binary_num}")
