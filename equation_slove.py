085ffd251cb9c81a412b9ba4af82dfd345199c81
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-16 20:52:22 +0530

    Add files via upload

---
+++ equation_slove.py
@@ -0,0 +1,7 @@
+# Write a program in python slove this question (a+a/2)= ans and the ans is even or odd
+a=float(input("Enter any value:"))
+b=a+(a/5)
+if (b%2==0):
+    print("even")
+else:
+    print("odd")
