2c630b2cbf6fc766cf9419f192e0c42d42ab13cd
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-20 10:04:21 +0530

    Add files via upload

---
+++ multification_table.py
@@ -0,0 +1,9 @@
+i = int(input("Enter any number: "))
+j = 1
+
+while j <= 10:
+    multiplication = j * i
+    print(f"{i} x {j} = {multiplication}")
+    j += 1
+
+                                                           
