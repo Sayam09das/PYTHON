e29d7c9dbe60233dc461282e894c9ad670bbba19
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-23 19:08:30 +0530

    Add files via upload

---
+++ swapping_using_3rd_ver.py
@@ -0,0 +1,8 @@
+# Write a python program to swap two numbers  using 3rd verabile
+a=int(input("Enter the 1st value: "))
+b=int(input("Enter the 2nd value: "))
+print(f"Before swapping the value is:{a} and {b}")
+c=a
+a=b
+b=c
+print(f"After swaping the is:{a} and {b}")
