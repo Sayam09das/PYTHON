71c3e9e9a0d730138afec2473c7e9303cc19242d
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-23 19:07:40 +0530

    Add files via upload

---
+++ Reversr_the_accepted_from_the_user.py
@@ -0,0 +1,11 @@
+# Write a program to reverse the number a accepted from usr using while loop
+
+num = int(input("Enter any number: "))
+num2 = 0
+
+while num != 0:
+    rev = num % 10
+    num2 = num2 * 10 + rev
+    num = num // 10
+
+print("Reverse number is:", num2)
