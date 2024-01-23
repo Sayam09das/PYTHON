5680723107d2c23c99f4ecd90a0a044868894c49
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-01-23 19:09:08 +0530

    Add files via upload

---
+++ the_product_of_digit.py
@@ -0,0 +1,10 @@
+# Write a program to find the product of the digit of a number accepted from the user
+
+num=int(input("Enter a number: "))
+product=1
+while num>0:
+    digit=num%10
+    product *=digit
+    num //= 10  # Remove the last digit from the number
+
+print("The product of the digits is: ",product)
