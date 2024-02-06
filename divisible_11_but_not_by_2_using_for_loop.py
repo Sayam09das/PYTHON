92507f7baf13331024f1852c402051ec3e5a5968
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-06 19:55:22 +0530

    Add files via upload

---
+++ divisible_11_but_not_by_2_using_for_loop.py
@@ -0,0 +1,5 @@
+# Write a program to diplay all the numbers which are divible by 11 but not by 2 between 100 and 500.
+
+for i in range(100,500):
+    if i%11==0 and i%2!=0:
+        print(i,end=' ')
