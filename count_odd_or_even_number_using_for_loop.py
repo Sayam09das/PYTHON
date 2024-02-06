081e2afe69a7df7e0997dd99c10778d978894018
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-06 22:34:20 +0530

    Add files via upload

---
+++ count_odd_or_even_number_using_for_loop.py
@@ -0,0 +1,12 @@
+start=int(input("Enter start number: "))
+end=int(input("Enter end number: "))
+sum_of_odd=0
+sum_of_even=0
+for i in range(start,end):
+    if i%2==0:
+        sum_of_even+=1
+    else:
+        sum_of_odd+=1
+
+print("Sum of all even number is",sum_of_even) 
+print("Sum of all odd number is",sum_of_odd)
