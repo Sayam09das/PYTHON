8ad815b584077f9519af5c660b4bf5c91544be67
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-07 19:39:40 +0530

    Add files via upload

---
+++ print_series_using_nested_for_loop.py
@@ -0,0 +1,12 @@
+# Write a program in python to print the series.
+#   *
+#   * *
+#   * * *
+#   * * * *
+#   * * * * *
+
+rows=5
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print(" ")
