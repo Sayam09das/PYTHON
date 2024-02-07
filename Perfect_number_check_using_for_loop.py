de1dba00bd7958ae427012d33265a5da8ca40a1a
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-07 18:55:30 +0530

    Add files via upload

---
+++ Perfect_number_check_using_for_loop.py
@@ -0,0 +1,14 @@
+# Write a program to accept a number and check whether it is a perfect number or not.
+# (Perfect number is a positive integer which is equal to the sum of its divisors like divisors of 6 are 1,2,3)
+# and of divisiors is also 6, so 6 is perfect number)
+
+number=int(input("Enter any number: "))
+s=0
+for i in range(1,number):
+    if number%i==0:
+        s=s+i
+
+if number==s:
+    print("NUMBER IS PERFECT")
+else:
+    print("NUMBER IS NOT PERFECT")
