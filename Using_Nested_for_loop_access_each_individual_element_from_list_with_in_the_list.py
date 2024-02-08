82d2d816da233278d53dfc2dff27edc4d60a89cc
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-09 00:41:01 +0530

    Add files via upload

---
+++ Using_Nested_for_loop_access_each_individual_element_from_list_with_in_the_list.py
@@ -0,0 +1,6 @@
+# Using Nested for loop access each individual element from list with in the list
+
+list=[['Lion','Deer','Sheep'],['Cub','Fawn','Lamp']]
+for i in list:
+    for j in i:
+        print(j)
