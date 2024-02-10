3a86a549fa5907c8b79158abd302d8c21f23e272
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-10 20:43:01 +0530

    Add files via upload

---
+++ convert_F_to_C_using_function.py
@@ -0,0 +1,8 @@
+# Write a program to convert F to C .
+
+def convert():
+    F=float(input("Enter the temperature in fahreheit:"))
+    C=(0.56)*(F-32)
+    print("Temperature in degrees celsius:",C)
+
+convert()
