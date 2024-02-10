0866ae3fc289cff3f2eac71b769b0bd4710efa12
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-10 23:37:06 +0530

    Add files via upload

---
+++ Check_number_equal_or_not_using_function.py
@@ -0,0 +1,13 @@
+# Write a progarm in python to check a number is greater then or lessthen or equal
+
+def number():
+    x=int(input("Enter the 1st number: "))
+    y=int(input("Enter the 2nd number: "))
+    if x>y:
+        print(x, "is greater then",y)
+    elif x<y:
+        print(x, "is lessthen",y)
+    else:
+        print("both are equal",x,y)
+
+number()
