0fbc7d84ca4f771d3a772307db4ef5cfa2407e65
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-11 14:38:28 +0530

    Add files via upload

---
+++ The_sum_of_1st_10_number_using_function.py
@@ -0,0 +1,10 @@
+# Write a python program to calculate the sum of first 10 numbers.
+def result():
+    i=1
+    sum=0
+    while i<=10:
+        sum+=i
+        i=i+1
+    print(f"Sum is:{sum}")
+
+result()
