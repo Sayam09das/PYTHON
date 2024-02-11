e6423bddbb8ced529d160d39e8c2ce48b3554340
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-11 11:42:49 +0530

    Add files via upload

---
+++ Swap_two_number_using_function.py
@@ -0,0 +1,11 @@
+# Write a python program to swap two number without 3rd veriable
+def swap():
+    a=int(input("Enter the 1st value: "))
+    b=int(input("Enter the 2nd value: "))
+    print(f"Before swapping the value is: {a} and {b}")
+    a=a+b
+    b=a-b
+    a=a-b
+    print(f"After swaping the is: {a} and {b}")
+
+swap()
