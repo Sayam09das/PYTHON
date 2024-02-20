949482dd5c0b6fc147beaf28ca0396d412448d23
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-20 19:25:05 +0530

    Add files via upload

--- multification_table_using_for_loop.py
+++ multification_table_using_for_loop.py
@@ -1,7 +1,15 @@
+def multi():
+    for i in range(1,11):
+        multiplication =number*i  
+        print(f"{number} x {i} = {multiplication}")
+
 number = int(input("Enter any number: "))
-for i in range(1,11):  #[start, stop]) mean 1 is start and 11 end value. 
-                        #     So, range(1, 11) includes the numbers 1, 2, 3, ..., 10. 
-                        #     If you wanted to print the multiplication table up to a different number,
-                        #     you could adjust the range accordingly.
-    multiplication =number*i  
-    print(f"{number} x {i} = {multiplication}")
+multi()
+
+
+
+# The range in the for loop, range(1, 11), is used to iterate from 1 to 10 (inclusive). This is because you typically want to print the multiplication table from 1 to 10. The range is specified as [start, stop), where start is inclusive and stop is exclusive.
+
+# So, range(1, 11) includes the numbers 1, 2, 3, ..., 10. If you wanted to print the multiplication table up to a different number, you could adjust the range accordingly.
+
+# For example, if you wanted to print the multiplication table up to 12, you would use range(1, 13):
