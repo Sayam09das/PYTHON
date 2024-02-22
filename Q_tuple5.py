a83b3862f735dac07b25e8b2f0313a51061fd1bc
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-22 19:32:39 +0530

    Add files via upload

---
+++ Q_tuple5.py
@@ -0,0 +1,7 @@
+# Check if an element exists in a tuple.
+my_tuple = (1, 2, 3, 4, 5)
+element = 3
+if element in my_tuple:
+    print("Element", element, "exists in the tuple")
+else:
+    print("Element", element, "does not exist in the tuple")
