427c209f4309df8960187f04bb1188555c606eb9
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-22 19:39:55 +0530

    Add files via upload

---
+++ Q_tuple9.py
@@ -0,0 +1,6 @@
+# Find the maximum and minimum elements in a tuple
+my_tuple = (1, 2, 3, 4, 5)
+maximum = max(my_tuple)
+minimum = min(my_tuple)
+print("Maximum element:", maximum)
+print("Minimum element:", minimum)
