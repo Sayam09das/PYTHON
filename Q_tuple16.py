ba8f4ed670e629d937b120a08459266eb1299ac8
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-25 11:13:19 +0530

    Add files via upload

---
+++ Q_tuple16.py
@@ -0,0 +1,6 @@
+# Find the first and last elements of a tuple:
+my_tuple = (1, 2, 3, 4, 5)
+first_element = my_tuple[0]
+last_element = my_tuple[-1]
+print("First element:", first_element)
+print("Last element:", last_element)
