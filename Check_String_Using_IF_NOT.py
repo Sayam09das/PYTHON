7d92f63c0b7383ab2886b31865b63d282c51468f
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2025-04-05 20:00:49 +0530

    Add files via upload

---
+++ Check_String_Using_IF_NOT.py
@@ -0,0 +1,6 @@
+# Check String
+# Check if NOT
+# To check if a certain phrase or character is NOT present in a string, we can use the keyword not in.
+
+txt = "The best things in life are free!"
+print("Hello World" not in txt)
