ec223e5c23ee7f1adbb76b50fadc3d90b053b5e2
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2025-04-05 19:57:39 +0530

    Add files via upload

---
+++ Check_String_Using_IF.py
@@ -0,0 +1,6 @@
+# Check String
+# To check if a certain phrase or character is present in a string, Use it in an if statement:
+
+txt = "The best things in life are free!"
+if "best" in txt:
+    print("Yes, 'best' is present.")
